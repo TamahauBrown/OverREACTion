d49e0940582a2ccc3dc85efa02fb5b43d8c9f93a
tamahau.brown <tamahau.brown@tidyint.com>
2022-07-17 21:39:28 +1200

    Adding Sol information

--- src/Components/Sol/Sol.tsx
+++ src/Components/Sol/Sol.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../../App.css';
+import './Sol.css'
 import {Header} from '../../GlobalElements/Header'
 import {Footer} from "../../GlobalElements/Footer";
 
@@ -8,7 +9,21 @@ export function Sol() {
         <div className="App">
             <Header/>
             <body className="App-body">
-            Welcome to Sol!
+            <div className="SolDescription">
+            <div className="introduction"><h1>Sol</h1></div>
+            <div className="description">
+                <h6>A long time ago or so the stories are mentioned, the power of magic ruled over the world of Sol.
+                The four elements known as the power harnessed by elementalists created the base of the world that
+                humans harnessed to thrive and develop civilizations leading to prosperity or so it seemed... Throughout
+                the course of time, man's dreams created untold strife, dreams of endless riches, eternal life or
+                domination over all that lived... dreams of conquest and unfortunately... of war.  The world would have
+                been torn apart if not for a few brave and wise heroes who sealed the full extent of elemental power away
+                in the Mt. Lunaris's, Luna Sanctum in the form of 4 elemental stars. Mt. Lunaris is located in the city
+                of Solgaleo. Three years ago in the city of Solgaleo, an attempt was made to harness the power, as an
+                attempt to steal the stars which caused untold destruction, fortunately the power was sealed away but
+                    many lost their parents in the disturbance.</h6>
+            </div>
+            </div>
             </body>
             <Footer/>
         </div>
--- src/GlobalElements/Header.tsx
+++ src/GlobalElements/Header.tsx
@@ -2,24 +2,19 @@ import React from 'react';
 import '../App.css';
 import {Link} from 'react-router-dom'
 import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 
 export function Header() {
     return (
     <header className="App-header">
         <Link to="../" id="header-item"> Home </Link>
 
-        <Dropdown>
-            <Dropdown.Toggle variant="success" id="header-link">
-                Worlds
-            </Dropdown.Toggle>
-
-        <Dropdown.Menu>
+        <DropdownButton id="header-link" title="Worlds">
             <Dropdown.Item><Link to='../aeryaan' id="header-item">Aeryaan</Link></Dropdown.Item>
             <Dropdown.Item><Link to='../serenity' id="header-item">Serenity</Link></Dropdown.Item>
             <Dropdown.Item><Link to='../kava' id="header-item">Kava</Link> </Dropdown.Item>
             <Dropdown.Item><Link to='../sol' id="header-item">Sol</Link></Dropdown.Item>
-        </Dropdown.Menu>
-        </Dropdown>
+        </DropdownButton>
 
         <Link to='../DMNotes' id="header-item">DM Notes</Link>
     </header>
